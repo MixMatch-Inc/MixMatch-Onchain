e92a66cbe99f08becd1daf206d36fd3edd21581b
Zaharadeen <eesahzaharadeen@gmail.com>
2026-01-28 18:16:06 +0100

    Update account.service.ts

--- apps/stellar-service/src/services/account.service.ts
+++ apps/stellar-service/src/services/account.service.ts
@@ -4,10 +4,25 @@ export const checkAccount = async (publicKey: string) => {
   try {
     const account = await server.loadAccount(publicKey);
 
-    const balances = account.balances.map((b) => ({
-      asset: b.asset_type === 'native' ? 'XLM' : b.asset_code,
-      balance: b.balance,
-    }));
+    const balances = account.balances.map((b) => {
+      if (b.asset_type === 'native') {
+        return {
+          asset: 'XLM',
+          balance: b.balance,
+        };
+      }
+      if (b.asset_type === 'liquidity_pool_shares') {
+        return {
+          asset: 'Liquidity Pool', 
+          balance: b.balance,
+        };
+      }
+    
+      return {
+        asset: b.asset_code,
+        balance: b.balance,
+      };
+    });
 
     return {
       exists: true,
